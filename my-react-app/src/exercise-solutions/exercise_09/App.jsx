2670dd03c9db2aa7c3a368c0d07e8bcb84bb2555
johncfrauen-lc101 <christian.frauenhoffer.cs@launchcode.org>
2024-11-09 15:33:26 -0700

    worked thru exercise 11

--- my-react-app/src/exercise-solutions/exercise_09/App.jsx
+++ my-react-app/src/exercise-solutions/exercise_09/App.jsx
@@ -5,21 +5,18 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import UserProfile from "./pages/UserProfile";
 import NotFound from "./pages/NotFound";
-import SimpleForm from "./components/SimpleForm";
 
 function App() {
   return (
     <div>
       <nav>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link> |
-        <Link to="/user/1">User 1</Link> | <Link to="form">Simple Form</Link>
+        <Link to="/">Home</Link> | <Link to="/about">About</Link> |{" "}
+        <Link to="/user/1">User 1</Link>
       </nav>
-
       <hr />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/form" element={<SimpleForm />} />
         <Route path="/user/:userId" element={<UserProfile />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
